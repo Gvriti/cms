9e0c1896c03c5f35974a9acdf448ac830307e0c6
David Gvritishvili <gvritishvili.david@gmail.com>
2016-03-05 15:25:11 +0400

    Remove language prepending to the title

--- app/Models/Traits/LanguageTrait.php
+++ app/Models/Traits/LanguageTrait.php
@@ -144,20 +144,10 @@ trait LanguageTrait
 
         $languages = languages();
 
-        $currentLanguage = language();
-
-        $title = isset($attributes['title']) ? $attributes['title'] : null;
-
         $attributes[$this->getForeignKey()] = $this->id;
 
         foreach($languages as $key => $value) {
             $attributes['language'] = $key;
-
-            if (! is_null($title) && $key != $currentLanguage) {
-                $attributes['title'] = $title.' ('.strtoupper($key).')';
-            } else {
-                $attributes['title'] = $title;
-            }
 
             $newLanguages[] = $this->language($this)->fill($attributes)->save();
         }
