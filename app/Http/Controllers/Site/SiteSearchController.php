29d70826f343d02e54b8751cb517788779301974
David Gvritishvili <gvritishvili.david@gmail.com>
2016-08-15 18:25:47 +0400

    Clean up search controller

--- app/Http/Controllers/Site/SiteSearchController.php
+++ app/Http/Controllers/Site/SiteSearchController.php
@@ -3,29 +3,10 @@
 namespace App\Http\Controllers\Site;
 
 use Models\Page;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class SiteSearchController extends Controller
 {
-    /**
-     * The Request instance.
-     *
-     * @var \Illuminate\Http\Request
-     */
-    protected $request;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     */
-    public function __construct(Request $request)
-    {
-        $this->request = $request;
-    }
-
     /**
      * Display the specified resource.
      *
@@ -36,9 +17,7 @@ class SiteSearchController extends Controller
     {
         $data['current'] = $page;
 
-        if ($this->request->has('q')) {
-            // do whatever you want
-        }
+        // do whatever you want
 
         return view('site.search', $data);
     }
