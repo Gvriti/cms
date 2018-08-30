a7468981b1eaea1637fe42d04cbc64c45103e472
David Gvritishvili <gvritishvili.david@gmail.com>
2018-08-30 16:56:49 +0400

    Update feedback mail template

--- resources/views/web/mail/feedback.blade.php
+++ resources/views/web/mail/feedback.blade.php
@@ -1,4 +1,14 @@
-<p>Name: {{$name}}</p>
-<p>Email: {{$email}}</p>
-<p>Phone: {{$phone}}</p>
-<p>Text: {{$text}}</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Feedback</title>
+</head>
+<body>
+    <p>Name: {{$name}}</p>
+    <p>Email: {{$email}}</p>
+    <p>Phone: {{$phone}}</p>
+    <p>Text: {{$text}}</p>
+</body>
+</html>
