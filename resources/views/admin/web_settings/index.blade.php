4b7be52e512d446fa1e037f434e60ba9134c07ba
David Gvritishvili <gvritishvili.david@gmail.com>
2019-05-03 19:50:26 +0400

    Remove unused assets

--- resources/views/admin/web_settings/index.blade.php
+++ resources/views/admin/web_settings/index.blade.php
@@ -101,22 +101,4 @@
     {!! Form::close() !!}
 @endif
 </div>
-@push('head')
-<link rel="stylesheet" href="{{ asset('assets/libs/js/select2/select2.css') }}">
-<link rel="stylesheet" href="{{ asset('assets/libs/js/select2/select2-bootstrap.css') }}">
-@endpush
-@push('body.bottom')
-<script src="{{ asset('assets/libs/js/select2/select2.min.js') }}"></script>
-<script type="text/javascript">
-$(function() {
-    $('.select').select2({
-        placeholder: 'Select type...',
-        allowClear: true
-    }).on('select2-open', function() {
-        // Adding Custom Scrollbar
-        $(this).data('select2').results.addClass('overflow-hidden').perfectScrollbar();
-    });
-});
-</script>
-@endpush
 @endsection
