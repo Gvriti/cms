a80c2d30eee2223fecf531279229c47123a18d45
David Gvritishvili <gvritishvili.david@gmail.com>
2020-07-31 19:23:35 +0400

    Update copy date

--- resources/views/web/_partials/footer.blade.php
+++ resources/views/web/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer">
     <div class="container">
-        <div class="copyright text-center">&copy; 2016</div>
+        <div class="copyright text-center">&copy; {{date('Y')}}</div>
         <!-- .copyright -->
     </div>
     <!-- .container -->
